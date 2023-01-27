3cd28abc6211df239f15a7ca005df1e7b01be347
Sergey Kuzmichev <kyz1979@mail.ru>
2023-01-28 00:29:20 +0300

    задача 2

---
+++ Z-2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая на вход принимает три числа и выдаёт, какое число большее, а какое меньшее.
+
+            Console.Write("Введите первое число ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите третье число ");
+            int c = Convert.ToInt32(Console.ReadLine());
+           
+           
+           int max=a;
+           
+            if (b>max)max=b;
+            if (c>max)max=c;
+
+           Console.WriteLine("макс число :{0}", max);
